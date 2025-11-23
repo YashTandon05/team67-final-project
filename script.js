3b9e630f82a7d474d6ee855ace419d69fef82274
Yash Tandon <yashtandon2005@gmail.com>
2025-11-22 18:13:33 -0800

    Added interactive globe that can be moved around

---
+++ script.js
@@ -0,0 +1,114 @@
+const width = 700;
+const height = 700;
+
+const svg = d3
+  .select("#globe-container")
+  .append("svg")
+  .attr("viewBox", `0 0 ${width} ${height}`)
+  .attr("preserveAspectRatio", "xMidYMid meet");
+
+const projection = d3.geoOrthographic().scale(width * 0.45).translate([width / 2, height / 2]).clipAngle(90);
+const path = d3.geoPath(projection);
+const graticule = d3.geoGraticule();
+
+svg
+  .append("path")
+  .datum({ type: "Sphere" })
+  .attr("class", "sphere")
+  .attr("d", path);
+
+svg
+  .append("path")
+  .datum(graticule())
+  .attr("class", "graticule")
+  .attr("d", path);
+
+
+const landGroup = svg.append("g").attr("class", "land-group");
+let isDragging = false;
+let inertiaRotationSpeed = 0;
+
+let lastDragTime = null;
+let lastDragX = null;
+let lastTime = Date.now();
+
+function redraw() {
+  svg.selectAll(".sphere").attr("d", path);
+  svg.selectAll(".graticule").attr("d", path);
+  landGroup.selectAll("path.land").attr("d", path);
+}
+
+const drag = d3.drag().on("start", (event) => {
+    isDragging = true;
+    svg.classed("dragging", true);
+
+    inertiaRotationSpeed = 0;
+    lastDragTime = Date.now();
+    lastDragX = event.x;
+  })
+  .on("drag", (event) => {
+    const rotate = projection.rotate();
+    const k = 0.25;
+
+    const newLambda = rotate[0] + event.dx * k;
+    const newPhi = rotate[1] - event.dy * k;
+    projection.rotate([newLambda, newPhi]);
+    redraw();
+
+    const now = Date.now();
+    const dt = now - lastDragTime || 16;
+    const xPixelsMoved = event.x - lastDragX; // we could imrpove this by also using the y coordinate
+    const degMoved = xPixelsMoved * k;
+
+    inertiaRotationSpeed = degMoved / dt;
+    lastDragTime = now;
+    lastDragX = event.x;
+  })
+  .on("end", () => {
+    isDragging = false;
+    svg.classed("dragging", false);
+
+    const maxInertiaSpeed = 1; 
+    if (inertiaRotationSpeed > maxInertiaSpeed) inertiaRotationSpeed = maxInertiaSpeed;
+    if (inertiaRotationSpeed < -maxInertiaSpeed) inertiaRotationSpeed = -maxInertiaSpeed;
+  });
+
+svg.call(drag);
+
+d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2.0.2/countries-110m.json")
+  .then((world) => {
+    const land = topojson.feature(world, world.objects.land);
+
+    landGroup.selectAll("path")
+      .data([land])
+      .join("path")
+      .attr("class", "land")
+      .attr("d", path);
+
+    animate();
+  })
+  .catch((error) => console.error("Map load error:", error));
+
+
+function animate() {
+  lastTime = Date.now();
+
+  d3.timer(() => {
+    const now = Date.now();
+    const dt = now - lastTime;
+    lastTime = now;
+
+    if (!isDragging) {
+      const rotate = projection.rotate();
+      const newLambda = rotate[0] + inertiaRotationSpeed * dt;
+      projection.rotate([newLambda, rotate[1], rotate[2] || 0]);
+      redraw();
+
+      const decay = 0.94;
+      inertiaRotationSpeed *= decay;
+      if (Math.abs(inertiaRotationSpeed) < 0.00001) {
+        inertiaRotationSpeed = 0;
+      }
+    }
+  });
+}
